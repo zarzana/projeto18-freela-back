74e2204260863b45b41762baf8f8ce263073e1f0
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-08-12 03:01:52 -0300

    POST/cat breed_id no longer accepts null

--- src/middleware/schemas/catSchemas.js
+++ src/middleware/schemas/catSchemas.js
@@ -3,7 +3,7 @@ import joi from "joi";
 export const postCatSchema = joi.object({
     cat_name: joi.string().pattern(/[a-zA-Z0-9 ]/).required(),
     available: joi.boolean().required(),
-    breed_id: joi.number().required().allow(null),
+    breed_id: joi.number().min(1).max(103).required(),
     male: joi.boolean().required().allow(null),
     birthday: joi.date().required().allow(null),
     description: joi.string().required().allow(null).allow(''),
