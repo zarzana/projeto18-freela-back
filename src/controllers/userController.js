d3dc190ebc1b7db2e7090085e0174a1d5484ab7e
Gabriel Zarzana Espinoza <gabriel.zarzana@gmail.com>
2023-08-14 17:27:08 -0300

    Removed GET/user/:id and added GET/user/cats

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -18,14 +18,40 @@ export async function getUserById(req, res) {
 
         let catArray = dbReponse.rows.map(({
             name, icon, email, phone, profile, ...keepAttrs
-            }) => keepAttrs);
-        if (catArray[0].cat_id === null) {catArray = []};
+        }) => keepAttrs);
+        if (catArray[0].cat_id === null) { catArray = [] };
         const userInformation = dbReponse.rows.map(({
             cat_id, cat_name, breed_name, male, birthday, description, ...keepAttrs
-            }) => keepAttrs)[0];
+        }) => keepAttrs)[0];
         userInformation.cats = catArray;
 
         res.status(200).send(userInformation);
+
+    } catch (error) {
+
+        res.status(500).send(error.message);
+
+    }
+
+}
+
+export async function getUserCats(req, res) {
+
+    const userId = res.locals.userId;
+
+    try {
+
+        const dbReponse = await db.query(`
+            SELECT cat_id, cat_name, available, breed_name, male, birthday, description
+            FROM "user"
+            LEFT JOIN "cat"
+                USING (user_id)
+            LEFT JOIN "breed"
+                USING (breed_id)
+            WHERE user_id = ${userId};
+        `);
+
+        res.status(200).send(dbReponse.rows);
 
     } catch (error) {
 
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { readAccessToken } from '../middleware/cookieMiddleware.js';
 import { verifyAcessToken } from '../middleware/jwtMiddleware.js';
-import { getUserById } from '../controllers/userController.js';
+import { getUserById, getUserCats } from '../controllers/userController.js';
 
 const router = Router();
 
-router.get('/api/user/:id', readAccessToken, verifyAcessToken, getUserById);
+// router.get('/api/user/:id', readAccessToken, verifyAcessToken, getUserById);
+router.get('/api/user/cats', readAccessToken, verifyAcessToken, getUserCats);
 
 export default router;
